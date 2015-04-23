cd0ed886fb5714f73434c204a9d2dfe285b85f51
Lucas Sabião <lucas.sabiao@hotmail.com>
2015-04-23 13:41:21 -0300

    Query na Web
    Adicionado código para procurar na lista da web

--- Where to Eat/restaurante.py
+++ Where to Eat/restaurante.py
@@ -1,6 +1,6 @@
-import random
+import random,urllib2
 
-lista = open("locais.txt")
+lista = urllib2.urlopen("https://raw.githubusercontent.com/dedig/utils/master/Where%20to%20Eat/locais.txt")
 tmp = lista.read()
 restaurantes = tmp.split("\n")
 random.shuffle(restaurantes)
